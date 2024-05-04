9376c7170790f05b132a2a0caf24251e99fde42f
3boood_pro <apoda980@gmail.com>
2024-05-04 21:54:17 +0300

    Revert "talked"
    This reverts commit 73cb6f46784757b98de42258e9f651dbdd7cf1cf.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        CustomArray myArray = new CustomArray();
+        Patient omar = new Patient("Omar", 19, 'M', "+966501111111", "Madinah, Near UPM.");
+        Doctor ahmed = new Doctor("Ahmad", 48, 'M', "General", new CustomArray());
+        CustomArray schedule = ahmed.getSchedule();
 
-        myArray.addElement(new Appointment(new Date(), new Patient("Khaled", 99, 'M', "+9606060", "Madinah")));
-        myArray.addElement(new String("h2"));
-        myArray.addElement(new String("h3"));
+        Appointment firstAppoint = new Appointment(new Date(2024, 5, 10), omar);
+        Appointment secondAppoint = new Appointment(new Date(2024, 5, 23), omar);
 
-
-        System.out.println((Appointment) myArray.getElement(0).getDate());
+        schedule.addElement(firstAppoint);
+        schedule.addElement(secondAppoint);
+        ahmed.setSchedule(schedule);
+        System.out.println((ahmed.getSchedule().toString()));
 
     }
 }
