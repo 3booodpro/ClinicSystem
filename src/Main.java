99f21fb40033564f6968e1d103b83dd53621367b
m01yyad <moayyadnum@gmail.com>
2024-05-04 21:59:55 +0300

    deleted somthing

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,6 @@ public class Main {
         Appointment firstAppoint = new Appointment(new Date(2024, 5, 10), omar);
         Appointment secondAppoint = new Appointment(new Date(2024, 5, 23), omar);
 
-        schedule.addElement(firstAppoint);
-        schedule.addElement(secondAppoint);
-        ahmed.setSchedule(schedule);
-        System.out.println((ahmed.getSchedule().toString()));
 
     }
 }
